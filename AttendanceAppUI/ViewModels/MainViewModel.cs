da47c0f33c0b3dd77d782eb72773cec04af7718d
johndward01 <johndward01@gmail.com>
2021-05-06 18:04:43 -0500

    changed MenuItem Headers

---
+++ AttendanceAppUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AttendanceAppUI.Views
+{
+    class MainViewModel : BaseViewModel
+    {
+    }
+}
