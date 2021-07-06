792818f6b8d5e2a7a6ff4e82342e15eea0b55aa5
johndward01 <johndward01@gmail.com>
2021-07-05 21:52:36 -0500

    deleted zoom model and parser class

--- AttendanceAppUI/Models/Student.cs
+++ AttendanceAppUI/Models/Student.cs
@@ -1,37 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AttendanceAppUI.Models
+﻿namespace AttendanceAppUI.Models
 {
     public class Student
     {
-        private int _totalMinutes = 0;
-
-        // propfull
-        private int _duration;
-
-        public int Duration
-        {
-            get 
-            { 
-                return _duration;
-            }
-            set 
-            {
-                _duration = value;
-                _totalMinutes += value;
-            }
-        }
-
         public string Name { get; set; }
         public string Email { get; set; }
-        public string CourseTitle { get; set; }
 
-        
-        public double TotalHours()
-        {
-            return _totalMinutes / 60;
-        }
     }
 }
